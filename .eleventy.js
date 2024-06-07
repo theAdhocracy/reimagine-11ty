cbe4b3bda2f951ec9718847801728a5b1fd46b8e
Murray Champernowne <murrayadcock@gmail.com>
2024-06-07 16:08:25 +0100

    feat: webc integration

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const pluginWebC = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("reset.css");
 	eleventyConfig.addPassthroughCopy("theme.css");
+	eleventyConfig.addPlugin(pluginWebC, {
+		components: "_includes/components/*.webc",
+	});
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
 };
